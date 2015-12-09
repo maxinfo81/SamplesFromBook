0556526c2467394120ea518ca56935c0b2675733
maxinfo81 <maxinfo81@gmail.com>
2015-12-09 23:17:13 +0200

    Add gitignore
    Add gitignore

---
+++ ClassInvestigation/MoneyEntry.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInvestigation
+{
+    class MoneyEntry
+    {
+        private double _amount;
+
+        public MoneyEntry()
+        {
+            _amount = 0;
+            EntryDate=DateTime.Now;
+        }
+
+        public MoneyEntry(double amount, DateTime date)
+        {
+            _amount = amount;
+            EntryDate = date;
+            
+        }
+
+        public void InitWithString(string amount, string date)
+        {
+            Double.TryParse(amount, out _amount);
+            DateTime dt;
+            DateTime.TryParse(date, out dt);
+            EntryDate = dt;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} from {1}", _amount, EntryDate.Date);
+        }
+        public bool isDebit 
+        {
+            get { return (_amount <= 0); }
+            set
+            {
+                if (value && _amount < 0)
+                    _amount = -_amount;
+                {
+                    
+                }
+            }
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+        public DateTime EntryDate { get; set; }
+    }
+}
---
+++ ClassInvestigation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInvestigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoneyEntry me = new MoneyEntry();
+
+            Console.WriteLine("Hello");
+            Console.Write("Enter sum: ");
+            string amount = Console.ReadLine();
+            Console.Write("Enter date: ");
+            string date = Console.ReadLine();
+            me.InitWithString(amount,date);
+
+            Console.Write("Your Entry: ");
+            Console.WriteLine(me.ToString());
+            if (me.isDebit) 
+            Console.WriteLine("You have debdt");
+            else
+            Console.WriteLine("You have income");
+            Console.ReadKey();
+        }
+    }
+}
